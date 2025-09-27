c3b69e5426014ce9bdbf881efd9cbffaba82d58f
dongkyunKim <kyun010713@gmail.com>
2025-09-27 13:54:37 +0900

    refactor(#105): 앨범 수정 http 메서드  patch로 변경

--- src/main/java/com/widyu/domain/album/api/AlbumController.java
+++ src/main/java/com/widyu/domain/album/api/AlbumController.java
@@ -22,9 +22,9 @@ import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -78,7 +78,7 @@ public class AlbumController implements AlbumDocs {
 
 
     @Override
-    @PutMapping("/{albumId}")
+    @PatchMapping("/{albumId}")
     public ApiResponseTemplate<AlbumUploadResponse> updateAlbum(
             @PathVariable Long albumId,
             @Valid @RequestBody AlbumUpdateRequest request
