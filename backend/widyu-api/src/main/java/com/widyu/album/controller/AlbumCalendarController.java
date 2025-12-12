166f9d44f55255501f945522759b090ea43eabd8
dongkyunKim <kyun010713@gmail.com>
2025-12-12 20:42:59 +0900

    refacotor(#155): api 엔드포인트 통일성 있게 변경

--- backend/widyu-api/src/main/java/com/widyu/album/controller/AlbumCalendarController.java
+++ backend/widyu-api/src/main/java/com/widyu/album/controller/AlbumCalendarController.java
@@ -15,7 +15,7 @@ import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api/v1/album/calendar")
+@RequestMapping("/api/v1/albums/calendar")
 public class AlbumCalendarController implements AlbumCalendarDocs {
 
     private final AlbumCalendarService albumCalendarService;
